886dc0baf6b6738ef5442388d3ddd7e28afccd0a
Michal Odnous <odiroot@users.noreply.github.com>
2026-05-07 21:25:13 +0200

    - remote-controls.h

--- ext/draft/draft.v
+++ ext/draft/draft.v
@@ -3,10 +3,12 @@ module draft
 pub const ext_cv = unsafe { (&char(C.CLAP_EXT_CV)).vstring() }
 pub const ext_preset_load = unsafe { (&char(C.CLAP_EXT_PRESET_LOAD)).vstring() }
 pub const ext_tuning = unsafe { (&char(C.CLAP_EXT_TUNING)).vstring() }
+pub const ext_remote_controls = unsafe { (&char(C.CLAP_EXT_REMOTE_CONTROLS)).vstring() }
 
 pub const draft_extensions = [
 	ext_cv,
 	ext_preset_load,
 	ext_tuning,
+	ext_remote_controls,
 ]
 

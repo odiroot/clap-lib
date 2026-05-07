78eab03216a96960abb9bc1ec8c978341a070f18
Michal Odnous <odiroot@users.noreply.github.com>
2026-05-07 21:16:24 +0200

    - tuning.h

--- README.md
+++ README.md
@@ -48,6 +48,5 @@ Still missing:
  - ext/draft/track-info.h
  - ext/draft/transport-control.h
  - ext/draft/triggers.h
- - ext/draft/tuning.h
 
 
--- ext/draft/draft.v
+++ ext/draft/draft.v
@@ -2,9 +2,11 @@ module draft
 
 pub const ext_cv = unsafe { (&char(C.CLAP_EXT_CV)).vstring() }
 pub const ext_preset_load = unsafe { (&char(C.CLAP_EXT_PRESET_LOAD)).vstring() }
+pub const ext_tuning = unsafe { (&char(C.CLAP_EXT_TUNING)).vstring() }
 
 pub const draft_extensions = [
 	ext_cv,
 	ext_preset_load,
+	ext_tuning,
 ]
 
